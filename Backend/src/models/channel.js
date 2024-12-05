993e6c73053f27873cca5127aad7be4d3583a184
Aleks <143895611+A1eksD@users.noreply.github.com>
2024-12-05 11:02:13 +0100

    added cannel model

---
+++ Backend/src/models/channel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Joi = require('joi')
+
+
+const channelSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 40,
+    },
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    cards: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Card',
+    }
+})
+
+
+function validateChannel(user) {
+    const schema = Joi.object({
+        title: Joi.string().min(3).max(40).required(),
+        creator: Joi.object().required(),
+        cards: Joi.object().optional(),
+    })
+    return schema.validate(user)
+}
+
+
+const Channel = mongoose.model('Channel', channelSchema)
+module.exports.validate = validateChannel
+module.exports = Channel;
