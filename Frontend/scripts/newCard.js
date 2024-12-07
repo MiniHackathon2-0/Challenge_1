2d069175adc7e077939ffef7c98f3321e389344e
John Fieweger <john.fieweger98@gmail.com>
2024-12-07 11:17:45 +0100

    fix: createcardfetch error handler

--- Frontend/scripts/newCard.js
+++ Frontend/scripts/newCard.js
@@ -1,39 +1,38 @@
-function addNewCard(){
+function addNewCard() {
     let cardBox = document.getElementById("cardBox");
     cardBox.classList.remove("d_none");
     cardBox.innerHTML = newCardHTML();
 }
 
 
-function closeNewCard(event){
+function closeNewCard(event) {
     event.stopPropagation()
     let cardBox = document.getElementById("cardBox");
     cardBox.classList.add("d_none");
 }
 
 
-async function createCard(event){
+async function createCard(event) {
     event.preventDefault();
     event.stopPropagation()
-    
+
     try {
         const bodyData = collectData();
         const resp = await createCardFetch(bodyData);
-        
+
         console.log("Card created successfully:", resp);
         closeNewCard(event);
     } catch (error) {
         console.error("Error during card creation:", error);
+        alert(error.message)
     }
 }
 
 
-function collectData(){
+function collectData() {
     const QUESTION = document.getElementById("front").value;
     const ANSWER = document.getElementById("back").value;
     const COLOR = document.getElementById("color").value;
-    const CREATOR = localStorage.getItem("id");
-    const DATE = Date.now();
     const POSX = Math.random();
     const POSY = Math.random();
     const CATEGORY = localStorage.getItem("curentCategory") || "Unterricht";
@@ -41,10 +40,8 @@ function collectData(){
 
     return {
         question: QUESTION,
-        answer: ANSWER, 
-        creator: CREATOR,
+        answer: ANSWER,
         color: COLOR,
-        date: DATE,
         posY: POSY,
         posX: POSX,
         category: CATEGORY
@@ -62,6 +59,10 @@ async function createCardFetch(card) {
         },
         body: JSON.stringify(card)
     });
+
+    if (!response.ok) {
+        throw new Error(`HTTP-Error: ${response.status} ${response.statusText}`);
+    }
 
     return await response.json();
 }
